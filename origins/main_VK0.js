3a0159f456c7fbb20112bec8b5a7a84d79bc9de8
Pavel Kladkevich <kladpav@gmail.com>
2026-06-29 22:33:44 +0300

    fix: fixed fetching data

---
+++ origins/main_VK0.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         AutoRepVK_0_mod05_remote
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Connect external script
+// @author       Pavel
+// @match        https://vk.com/*
+// @grant        none
+// @run-at       document-end
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=vk.com
+// @require      https://www.gstatic.com/firebasejs/12.7.0/firebase-app-compat.js
+// @require      https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore-compat.js
+// @require      https://raw.githubusercontent.com/kladweb/autoposting-scripts/refs/heads/main/build/VK0_fin.js
+// ==/UserScript==
